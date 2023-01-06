51b39770f71ef982276a54f8a5d7894abbcc9b6e
Mat-Hatter <cloverro84@gmail.com>
2023-01-05 21:39:55 -0500

    Version 1.0 Release

---
+++ Source/ProjectZero/ProjectZero.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectZero.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectZero, "ProjectZero" );
---
+++ Source/ProjectZero/ProjectZeroGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectZeroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZERO_API AProjectZeroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
